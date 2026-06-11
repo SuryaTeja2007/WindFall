1a6495aaff03b4830d20f456a19bd5877a341561
vishnu83455 <vishnuvardhan83455@gmail.com>
2026-06-11 14:59:49 +0530

    pause ,restart,win screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,51 +1,133 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject winScreen;
-    public GameObject loseScreen;
     public GameObject startScreen;
+    public GameObject winScreen;
+    public GameObject pauseScreen;
+    public GameObject pauseButton;
 
+    public TextMeshProUGUI timerText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float timeElapsed = 0f;
+    private bool timerRunning = false;
+    private bool gamePaused = false;
+
     void Start()
     {
-        //if (winScreen != null)
-        //    winScreen.SetActive(false);
-
-        //if (loseScreen != null)
-        //    loseScreen.SetActive(false);
+        Time.timeScale = 0f;
 
         if (startScreen != null)
             startScreen.SetActive(true);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void StartGame()
-    {
-        if(startScreen != null)
+        if (winScreen != null)
+            winScreen.SetActive(false);
+
+        if (pauseScreen != null)
+            pauseScreen.SetActive(false);
+
+        if (pauseButton != null)
+            pauseButton.SetActive(false);
+
+        if (timerText != null)
         {
-            startScreen.SetActive(false);
+            timerText.text = "00:00";
+            timerText.gameObject.SetActive(false);
         }
     }
 
-    //public void WinScreen()
-    //{
-    //    if (winScreen != null)
-    //    {
-    //        winScreen.SetActive(true);
-    //    }
-    //}
+    void Update()
+    {
+        // Test Win Screen with P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShowWinScreen();
+        }
 
-    //public void LoseScreen()
-    //{
-    //    if (loseScreen != null)
-    //    {
-    //        loseScreen.SetActive(true);
-    //    }
-    //}
+        // Timer
+        if (timerRunning)
+        {
+            timeElapsed += Time.deltaTime;
+
+            int minutes = Mathf.FloorToInt(timeElapsed / 60);
+            int seconds = Mathf.FloorToInt(timeElapsed % 60);
+
+            if (timerText != null)
+            {
+                timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+        }
+
+        // ESC Pause / Resume
+        if (Input.GetKeyDown(KeyCode.Escape) && !startScreen.activeSelf)
+        {
+            if (gamePaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        if (startScreen != null)
+            startScreen.SetActive(false);
+
+        if (pauseButton != null)
+            pauseButton.SetActive(true);
+
+        if (timerText != null)
+            timerText.gameObject.SetActive(true);
+
+        timerRunning = true;
+        gamePaused = false;
+
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        if (pauseScreen != null)
+            pauseScreen.SetActive(true);
+
+        timerRunning = false;
+        gamePaused = true;
+
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        if (pauseScreen != null)
+            pauseScreen.SetActive(false);
+
+        timerRunning = true;
+        gamePaused = false;
+
+        Time.timeScale = 1f;
+    }
+
+    public void ShowWinScreen()
+    {
+        timerRunning = false;
+
+        if (pauseButton != null)
+            pauseButton.SetActive(false);
+
+        if (winScreen != null)
+            winScreen.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().buildIndex
+        );
+    }
 }
